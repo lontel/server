7e7b0f1400cbcf01a0acb6da126227b96a74920f
Javier Ruiz <javierdeveloper1@gmail.com>
2022-07-18 16:50:56 +0200

    index routes remove comments

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,7 +6,7 @@ router.get("/", (req, res, next) => {
 
 router.use('/account', require('./account.routes'))
 
-router.use('event', require('../routes/event.routes'))
+router.use('/event', require('../routes/event.routes'))
 
 
 module.exports = router
