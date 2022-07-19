63d85edfcaab7837d26ff73e2410cd1f1b0fe7ca
lontel <lonte.leon@gmail.com>
2022-07-19 17:15:06 +0200

    some changes on the event routes file

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -4,7 +4,7 @@ const Event = require('../models/Event.model')
 const { findByIdAndDelete } = require("../models/User.model")
 const { isAuthenticated } = require('../middleware/jwt.middleware')
 
-router.use('/getAllEvents', (req, res, next) => {
+router.get('/getAllEvents', (req, res, next) => {
 
     Event
         .find()
@@ -12,7 +12,7 @@ router.use('/getAllEvents', (req, res, next) => {
         .catch(err => res.status(500).json(err))
 })
 
-router.use('/getOneEvent/:event_id', isAuthenticated, (req, res, next) => {
+router.get('/getOneEvent/:event_id', isAuthenticated, (req, res, next) => {
 
     const { event_id } = req.params
 
@@ -22,16 +22,16 @@ router.use('/getOneEvent/:event_id', isAuthenticated, (req, res, next) => {
         .catch(err => res.status(500).json(err))
 })
 
-router.post('/saveEvent', isAuthenticated, (req, res) => {
+router.post('/saveEvent', (req, res) => {
 
-    const { event_id: owner } = req.payload
+
     Event
-        .create({ owner, ...req.body })
+        .create(req.body)
         .then(response => res.json(response))
-        .catch(err => res.status(500).json(err))
+        .catch(err => console.log(err))
 })
 
-router.put('/updateEvent/:event_id',isAuthenticated, (req, res, next) => {
+router.put('/updateEvent/:event_id', isAuthenticated, (req, res, next) => {
 
     const { event_id } = req.params
     const { origin, location, destination, date, description, numberOfCyclists, owner, cyclists } = req.body
