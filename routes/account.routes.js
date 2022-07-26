d06d501d72265d5f1752ca4a9ad259422d7bf209
lontel <lonte.leon@gmail.com>
2022-07-26 21:00:07 +0200

    adding  my profile route

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -3,6 +3,18 @@ const { isAuthenticated } = require('../middleware/jwt.middleware')
 
 const User = require('../models/User.model')
 
+
+
+router.get('/myprofile/:account_id', (req, res, next) => {
+    
+    const { account_id } = req.params
+
+    User
+        .findById(account_id)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
 router.get('/getAllAccounts', (req, res, next) => {
 
     User
